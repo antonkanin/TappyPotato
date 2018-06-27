c41fc48b1b3393db44daa242df6d5feed7243058
antonkanin <antonkanin@users.noreply.github.com>
2018-06-27 11:21:08 +0300

    updating GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,77 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
+    public delegate void GameDelegate();
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public static event GameDelegate OnGameStarted;
+    public static event GameDelegate OnGameOverConfirmed;
+
+    public static GameManager Instance;
+
+    public GameObject startPage;
+    public GameObject gameOverPage;
+    public GameObject countDownPage;
+    public Text scoreText;
+
+    enum PageState
+    {
+        None, 
+        Start,
+        GameOver,
+        Countdown
+    }
+
+    private int score = 0;
+    private bool gameOver = false;
+
+    public bool GameOver
+    {
+        get { return gameOver; }
+    }
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void SetPageState(PageState state)
+    {
+        switch (state)
+        {
+            case PageState.None:
+                startPage.SetActive(false);
+                gameOverPage.SetActive(false);
+                countDownPage.SetActive(false);
+                break;
+            case PageState.Start:
+                startPage.SetActive(true);
+                gameOverPage.SetActive(false);
+                countDownPage.SetActive(false);
+                break;
+            case PageState.GameOver:
+                startPage.SetActive(false);
+                gameOverPage.SetActive(true);
+                countDownPage.SetActive(false);
+                break;
+            case PageState.Countdown:
+                startPage.SetActive(false);
+                gameOverPage.SetActive(false);
+                countDownPage.SetActive(true);
+                break;
+        }
+    }
+
+    public void ConfirmGameOver()
+    {
+        // activated when replay button is hit
+    }
+
+    public void StartGame()
+    {
+        // activated when play button is hit
+    }
 }
