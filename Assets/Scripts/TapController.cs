05775cd7b21f6bb1b6a4971e6ebaaee007b435cd
antonkanin <antonkanin@users.noreply.github.com>
2018-06-27 11:11:59 +0300

    adding TapController and GameManager

--- Assets/Scripts/TapController.cs
+++ Assets/Scripts/TapController.cs
@@ -2,15 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TapController : MonoBehaviour {
+[RequireComponent(typeof(Rigidbody2D))]
+public class TapController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float tapForce = 10;
+    public float tiltSmooth = 5;
+    public Vector3 startPos;
+
+    private new Rigidbody2D rigidbody;
+    private Quaternion downRotation;
+    private Quaternion forwardRotation;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        downRotation = Quaternion.Euler(0, 0, -90);
+        forwardRotation = Quaternion.Euler(0, 0, 35);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Input.GetMouseButtonDown(0)");
+            transform.rotation = forwardRotation;
+            rigidbody.velocity = Vector3.zero;
+            rigidbody.AddForce(Vector2.up * tapForce, ForceMode2D.Force);
+        }
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, downRotation,
+            tiltSmooth * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "ScoreZone")
+        {
+            // register score event
+            // play sound
+        }
+
+        if (collider.gameObject.tag == "DeadZone")
+        {
+            rigidbody.simulated = false;
+            // register a dead event
+            // play a sound
+        }
+    }
+
 }
