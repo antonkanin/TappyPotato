1006f7921a556f0a596f6ff07498658a0ae322af
antonkanin <antonkanin@users.noreply.github.com>
2018-06-29 01:19:43 +0300

    #2 work in progress with JSON

--- Assets/Scripts/HighscoreText.cs
+++ Assets/Scripts/HighscoreText.cs
@@ -1,18 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.JsonUtility;
 using Utils;
 
 [RequireComponent(typeof(Text))]
 public class HighscoreText : MonoBehaviour
 {
+    [Serializable]
     class Player
     {
         public string Name;
-        public int Score;
+        public string Score;
     }
+
+    [Serializable]
+    class ScoreBoard
+    {
+        public List<Player> Items;
+    }
+
 
     private Text highScore;
 
@@ -21,10 +29,31 @@ public class HighscoreText : MonoBehaviour
         highScore = GetComponent<Text>();
         highScore.text = "High Score: " + PlayerPrefs.GetInt("HighScore").ToString();
 
-        string url = "http://localhost/game.php";
+
+        ScoreBoard s = new ScoreBoard
+        {
+            Items = new List<Player>
+            {
+                new Player
+                {
+                    Name = "Anton",
+                    Score = "100"
+                },
+                new Player
+                {
+                    Name = "Andrew",
+                    Score = "1"
+                }
+            }
+        };
+
+        var r = JsonUtility.ToJson(s);
+
+        var url = "http://localhost/game.php";
 
         WWW gameStats = new WWW(url);
         StartCoroutine(RequestGameStats(gameStats));
+        
     }
 
     IEnumerator RequestGameStats(WWW www)
@@ -32,8 +61,20 @@ public class HighscoreText : MonoBehaviour
         yield return www;
 
         string jsonResult = www.text;
+        jsonResult = fixJson(jsonResult);
 
-        JsonHelper j;
 
+        var scoreBoard = JsonUtility.FromJson<ScoreBoard>(jsonResult);
+        //var scoreBoard = JsonHelper.FromJson<Player>(jsonResult);
+        foreach (var p in scoreBoard.Items)
+        {
+            Debug.Log(p.Name + " " + p.Score);
+        }
+    }
+
+    string fixJson(string value)
+    {
+        value = "{\"Items\":" + value + "}";
+        return value;
     }
 }
