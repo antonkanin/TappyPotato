612bfb4372e56dba07364782ac0f3fbd23b2a4da
antonkanin <antonkanin@users.noreply.github.com>
2018-06-28 22:15:18 +0300

    #2 adding JsonHelper class

--- Assets/Scripts/HighscoreText.cs
+++ Assets/Scripts/HighscoreText.cs
@@ -2,15 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.JsonUtility;
+using Utils;
 
 [RequireComponent(typeof(Text))]
 public class HighscoreText : MonoBehaviour
 {
+    class Player
+    {
+        public string Name;
+        public int Score;
+    }
+
     private Text highScore;
 
     void OnEnable()
     {
         highScore = GetComponent<Text>();
         highScore.text = "High Score: " + PlayerPrefs.GetInt("HighScore").ToString();
+
+        string url = "http://localhost/game.php";
+
+        WWW gameStats = new WWW(url);
+        StartCoroutine(RequestGameStats(gameStats));
+    }
+
+    IEnumerator RequestGameStats(WWW www)
+    {
+        yield return www;
+
+        string jsonResult = www.text;
+
+        JsonHelper j;
+
     }
 }
