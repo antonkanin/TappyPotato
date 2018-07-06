cbf7b05ee4eb92308396546f9e69dc79361fd7bf
antonkanin <antonkanin@users.noreply.github.com>
2018-07-06 20:10:40 +0300

    more work on the scoreboard

--- Assets/LoadScoreBoard.cs
+++ Assets/LoadScoreBoard.cs
@@ -8,16 +8,16 @@ public class LoadScoreBoard : MonoBehaviour
 {
     public GameObject playerScorePrefab;
 
-	// Use this for initialization
-	void Start ()
-	{
-	    var scoreBoard = ScoreUtils.ScoreReader.GetScore();
-	    foreach (var playerScore in scoreBoard)
-	    {
-	        GameObject scoreLine = (GameObject) Instantiate(playerScorePrefab);
-	        scoreLine.transform.SetParent(this.transform, false);
-	        scoreLine.transform.Find("PlayerText").GetComponent<Text>().text = playerScore.player;
-	        scoreLine.transform.Find("ScoreText").GetComponent<Text>().text = playerScore.score;
-	    }
-	}
-
+    // Use this for initialization
+    void Start()
+    {
+        var scoreBoard = ScoreUtils.ScoreReader.GetScore();
+        foreach (var playerScore in scoreBoard)
+        {
+            GameObject scoreLine = (GameObject) Instantiate(playerScorePrefab);
+            scoreLine.transform.SetParent(this.transform, false);
+            scoreLine.transform.Find("PlayerText").GetComponent<Text>().text = playerScore.player;
+            scoreLine.transform.Find("ScoreText").GetComponent<Text>().text = playerScore.score;
+        }
+    }
+}
