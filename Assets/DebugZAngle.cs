dc3caf46439567c144929e190f519efa62d1cb47
antonkanin <antonkanin@users.noreply.github.com>
2018-07-10 15:30:32 +0300

    updating potato sprites

---
+++ Assets/DebugZAngle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugZAngle : MonoBehaviour {
+
+	// Use this for initialization
+    public Text textAngleZ;
+
+	// Update is called once per frame
+	void Update ()
+	{
+	    textAngleZ.text = transform.eulerAngles.z.ToString();
+	}
+}
