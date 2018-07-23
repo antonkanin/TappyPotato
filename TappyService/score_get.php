b5eb6bf2f8d89e867dbc47acb9ec88dbc5180e00
Anton Kanin <antony.kanin@gmail.com>
2018-07-23 13:53:50 +0300

    #20 updating get score php service

--- TappyService/score_get.php
+++ TappyService/score_get.php
@@ -1,5 +1,25 @@
 <?php
-  $conn = new mysqli("localhost", "root", "", "tappy_potato");
+    echo 'test01';
+    echo '</br>';
+    
+    //$db_settings = parse_ini_file('..\private\config.ini');
+    $db_settings = parse_ini_file("private/config.ini");
+
+    echo 'test02';
+    echo '</br>';
+    var_dump($db_settings);
+    echo '</br>';
+
+    echo $db_settings['servername'];
+	echo $db_settings['username'];
+	echo $db_settings['password'];
+	echo $db_settings['db_name'];
+
+    $conn = new mysqli($db_settings['servername'], $db_settings['username'], $db_settings['password'], $db_settings['db_name']);
+
+
+
+  //$conn = new mysqli("localhost", "root", "", "tappy_potato");
  
   // Check connection
   if($conn->connect_error)
@@ -7,7 +27,7 @@
       die("ERROR: Could not connect. " . $conn->connect_error);
   }
  
-  $sql = "SELECT player, score FROM score_board ORDER BY score DESC, date DESC";
+  $sql = "SELECT player_name, score FROM score_board ORDER BY score DESC, date_created DESC";
   $result = $conn->query($sql);
   
   if (!$result)
