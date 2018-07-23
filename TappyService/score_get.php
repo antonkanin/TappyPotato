239c6887b4c5570a1a5077c7907131290957625e
antonkanin <antonkanin@users.noreply.github.com>
2018-07-23 18:46:05 +0300

    #20 removing debug code from the score_get service

--- TappyService/score_get.php
+++ TappyService/score_get.php
@@ -1,31 +1,13 @@
 <?php
-    echo 'test01';
-    echo '</br>';
-    
-    //$db_settings = parse_ini_file('..\private\config.ini');
-    $db_settings = parse_ini_file("private/config.ini");
-
-    echo 'test02';
-    echo '</br>';
-    var_dump($db_settings);
-    echo '</br>';
-
-    echo $db_settings['servername'];
-	echo $db_settings['username'];
-	echo $db_settings['password'];
-	echo $db_settings['db_name'];
+    $db_settings = parse_ini_file("../private/config.ini");
 
     $conn = new mysqli($db_settings['servername'], $db_settings['username'], $db_settings['password'], $db_settings['db_name']);
-
-
-
-  //$conn = new mysqli("localhost", "root", "", "tappy_potato");
  
   // Check connection
-  if($conn->connect_error)
-  {
-      die("ERROR: Could not connect. " . $conn->connect_error);
-  }
+    if($conn->connect_error)
+    {
+        die("ERROR: Could not connect. " . $conn->connect_error);
+    }
  
   $sql = "SELECT player_name, score FROM score_board ORDER BY score DESC, date_created DESC";
   $result = $conn->query($sql);
@@ -45,6 +27,11 @@
 	  }
 	  echo json_encode($resultArray);
   }
+    else 
+    {
+        echo "no records in the database";
+    }
+
   
   $conn->close();
 ?>
