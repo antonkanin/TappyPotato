245ef447a8708d776893894805555cc55de69d46
antonkanin <antonkanin@users.noreply.github.com>
2018-07-24 08:40:01 +0300

    #20 changing paths to a global service

--- Unity/Assets/Scripts/Constants.cs
+++ Unity/Assets/Scripts/Constants.cs
@@ -9,8 +9,8 @@ namespace Constants
         public const string SCORE_FIELD = "score";
         public const string ACCESS_TOKEN = "access_token";
 
-        public const string POST_URL = "http://localhost/tappyservice/score_post.php";
-        public const string GET_URL = "http://localhost/tappyservice/score_get.php";
+        public const string POST_URL = "http://antonkanin.com/tappyservice/score_post.php";
+        public const string GET_URL = "http://antonkanin.com/tappyservice/score_get.php";
 
         //public const string PLAYER_NAME_PREF = "PlayerName";
         public const string PLAYER_HIGH_SCORE_PREF = "HighScore";
