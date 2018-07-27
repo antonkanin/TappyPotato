42a82c8c4565dbd0f104393541e883967bc257a6
antonkanin <antonkanin@users.noreply.github.com>
2018-07-27 09:52:12 +0300

    #24 removing phpseclib library, we are using out of the box openssl_decrypt instead

--- TappyService/vendor/composer/autoload_files.php
+++ TappyService/vendor/composer/autoload_files.php
@@ -7,5 +7,4 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'c65d09b6820da036953a371c8c73a9b1' => $vendorDir . '/facebook/graph-sdk/src/Facebook/polyfills.php',
-    'decc78cc4436b1292c6c0d151b19445c' => $vendorDir . '/phpseclib/phpseclib/phpseclib/bootstrap.php',
 );
--- TappyService/vendor/composer/autoload_psr4.php
+++ TappyService/vendor/composer/autoload_psr4.php
@@ -6,6 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'phpseclib\\' => array($vendorDir . '/phpseclib/phpseclib/phpseclib'),
     'Facebook\\' => array($vendorDir . '/facebook/graph-sdk/src/Facebook'),
 );
