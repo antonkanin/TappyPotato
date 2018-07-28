d531ccbbedb5f819136a40f67c77656fe0ad3e32
antonkanin <antonkanin@users.noreply.github.com>
2018-07-28 23:37:36 +0300

    adding index column to the score_board table

--- TappyService/score_board_create.sql
+++ TappyService/score_board_create.sql
@@ -1,7 +1,14 @@
 CREATE TABLE `score_board` (
+  `number` int(11) NOT NULL AUTO_INCREMENT,
   `player_id` varchar(100) NOT NULL,
   `player_name` varchar(100) NOT NULL,
   `score` int(11) NOT NULL,
   `death_position` int(11) NOT NULL,
   `date_created` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `score_board`
+  ADD PRIMARY KEY (`number`);
+
+ALTER TABLE `score_board`
+  MODIFY `number` int(11) NOT NULL AUTO_INCREMENT;
