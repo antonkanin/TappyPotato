49b4887fea55e2e39d6dbcfdf283410230dcba89
antonkanin <antonkanin@users.noreply.github.com>
2018-07-28 23:51:23 +0300

    returning death_position from the server; returning only one record with max score for each player

--- TappyService/score_get.php
+++ TappyService/score_get.php
@@ -1,37 +1,33 @@
 <?php
-    $db_settings = parse_ini_file("../private/config.ini");
+	$db_settings = parse_ini_file("../private/config.ini");
 
-    $conn = new mysqli($db_settings['servername'], $db_settings['username'], $db_settings['password'], $db_settings['db_name']);
- 
-  // Check connection
-    if($conn->connect_error)
-    {
-        die("ERROR: Could not connect. " . $conn->connect_error);
-    }
- 
-  $sql = "SELECT player_name, score FROM score_board ORDER BY score DESC, date_created DESC";
-  $result = $conn->query($sql);
-  
-  if (!$result)
-  {
-	  trigger_error("Invalid query: " . $conn->error);
-  }
-  
-  $resultArray = array();
-  
-  if ($result->num_rows > 0)
-  {
-	  while($row = $result->fetch_assoc())
-	  {
-		  $resultArray[] = $row;
-	  }
-	  echo json_encode($resultArray);
-  }
-    else 
-    {
-        echo "no records in the database";
-    }
+	$conn = new mysqli($db_settings['servername'], $db_settings['username'], $db_settings['password'], $db_settings['db_name']);
 
-  
-  $conn->close();
+	// Check connection
+	if($conn->connect_error) {
+		die("ERROR: Could not connect. " . $conn->connect_error);
+	}
+
+	$sql = "select * from score_board where number in
+		(select number from (select max(score), number from score_board group by player_id) as t)";
+	
+	$result = $conn->query($sql);
+
+	if (!$result) {
+		trigger_error("Invalid query: " . $conn->error);
+	}
+
+	$resultArray = array();
+
+	if ($result->num_rows > 0) {
+		while($row = $result->fetch_assoc()) {
+			$resultArray[] = $row;
+		}
+		echo json_encode($resultArray);
+	}
+	else {
+		echo "no records in the database";
+	}
+
+	$conn->close();
 ?>
