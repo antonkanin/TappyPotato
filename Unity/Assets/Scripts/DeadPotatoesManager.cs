87641e194dafa56374680823b64b4cdf607c400e
antonkanin <antonkanin@users.noreply.github.com>
2018-07-29 11:48:52 +0300

    #22 DeadPotatoManager - work in progress

--- Unity/Assets/Scripts/DeadPotatoesManager.cs
+++ Unity/Assets/Scripts/DeadPotatoesManager.cs
@@ -1,16 +1,41 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using PlayerClasses;
 using UnityEngine;
 
-public class DeadPotatoesManager : MonoBehaviour {
+public class DeadPotatoesManager : MonoBehaviour
+{
+    public GameObject deadPotatoPrefab;
 
 	// Use this for initialization
 	void Start () {
-		
+		Configure();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    private void Configure()
+    {
+        ScoreManager.Instance.GetScoreAsync(LoadPlayers);
+    }
+
+    private void LoadPlayers(IList<Player> players)
+    {
+        foreach (var player in players)
+        {
+            Debug.Log("Death position : " + player.death_position);
+
+            GameObject go = Instantiate(deadPotatoPrefab) as GameObject;
+            Transform t = go.transform;
+            t.SetParent(transform);
+
+            float x = Convert.ToInt32(player.death_position) / 10;
+            t.position.x = x;
+
+        }
+    }
 }
