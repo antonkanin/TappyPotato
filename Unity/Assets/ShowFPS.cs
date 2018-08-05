81e9254994728687f1c93a87ebbfb0a14ccdef2b
Anton Kanin <antony.kanin@gmail.com>
2018-08-05 11:33:14 +0300

    #44 adding FPS indicator

---
+++ Unity/Assets/ShowFPS.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowFPS : MonoBehaviour {
+
+    // Use this for initialization
+    public float deltaTime;
+    private Text fpsText;
+
+    void Start()
+    {
+        fpsText = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+        float fps = 1.0f / deltaTime;
+        fpsText.text = Mathf.Ceil(fps).ToString();
+    }
+}
