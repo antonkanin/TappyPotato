4f8e014a3719e29064ddb24edeeb35f4590a1481
Andrei Kuleuski <kuljok@gmail.com>
2018-08-07 17:32:04 +0300

    #13: Add version info to the build

---
+++ TappyPotato/Assets/Scripts/Version.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Version : MonoBehaviour {
+	private void Start()
+	{
+		var text = GetComponent<Text>();
+		text.text = "Version: " + Application.version;
+	}
+}
