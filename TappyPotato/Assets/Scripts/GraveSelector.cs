9e6b0f9d43c72c80f277ce0ecec0e94efd6d2499
Andrei Kuleuski <kuljok@gmail.com>
2018-08-19 18:00:13 +0300

    #48: Design grave sprites: different graves have been added

---
+++ TappyPotato/Assets/Scripts/GraveSelector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraveSelector : MonoBehaviour
+{
+
+	private static Sprite[] _sprites;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		if (_sprites == null)
+		{
+			_sprites = Resources.LoadAll<Sprite>("rip/rip_sprite");
+		}
+		GetComponent<SpriteRenderer>().sprite = _sprites[Random.Range(0, 7)];
+	}
+}
