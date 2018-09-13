d32594ef2425ea986f9c060cbd76efef3538e96a
antonkanin <antonkanin@users.noreply.github.com>
2018-09-13 13:23:27 +0300

    #42 turning off Z contraint on the potato Collider2D; refactoring TiltController

--- TappyPotato/Assets/Scripts/Potato/TiltController.cs
+++ TappyPotato/Assets/Scripts/Potato/TiltController.cs
@@ -4,17 +4,17 @@ public class TiltController : BaseTappyController
 {
     const float tiltSpeed = 100.0f;
     private Quaternion downRotation;
-    private Rigidbody2D rigidbody;
+    private Rigidbody2D rb;
 
     void Start()
     {
         downRotation = Quaternion.Euler(0, 0, -40);
-        rigidbody = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     protected override void ActiveFixedUpdate()
     {
-        Quaternion rotation = Quaternion.RotateTowards(transform.rotation, downRotation, tiltSpeed * Time.deltaTime);
-        rigidbody.MoveRotation(rotation.eulerAngles.z);
+        Quaternion rot = Quaternion.RotateTowards(transform.rotation, downRotation, tiltSpeed * Time.deltaTime);
+        rb.MoveRotation(rot.eulerAngles.z);
     }
 }
