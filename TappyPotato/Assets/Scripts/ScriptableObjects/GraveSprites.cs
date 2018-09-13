58a5ef6241790c062e42d45d378e948d3a095a25
antonkanin <antonkanin@users.noreply.github.com>
2018-09-13 23:31:58 +0300

    Refactoring GraveSprites scriptable object - sprites are now added through the Unity Editor instead of being dynamically loaded from the Resources folder

--- TappyPotato/Assets/Scripts/ScriptableObjects/GraveSprites.cs
+++ TappyPotato/Assets/Scripts/ScriptableObjects/GraveSprites.cs
@@ -1,27 +1,11 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace TappyPotato.ScriptableObjects
 {
     [CreateAssetMenu]
     public class GraveSprites : ScriptableObject
     {
-        private Sprite[] _sprites;
-
-        public Sprite[] Sprites
-        {
-            get
-            {
-                if (_sprites == null || _sprites.Length == 0)
-                {
-                    var graveSprites = Resources.LoadAll<Sprite>("rip/rip_sprite");
-                    if (graveSprites != null && graveSprites.Length > 0)
-                    {
-                        _sprites = graveSprites;
-                    }
-                }
-
-                return _sprites;
-            }
-        }
+        public Sprite[] Sprites;
     }
 }
